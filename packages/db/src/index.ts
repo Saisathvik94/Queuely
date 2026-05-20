2228550b2980f804e9b088a592df04c7efc113ed
Sai Sathvik <saisathvik615@gmail.com>
2026-05-20 11:14:13 +0530

    refactor(docker): Streamline multi-stage Docker builds and monorepo packaging
    This commit refactors the Dockerfiles for `api` and `worker` to improve build efficiency, reduce image

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "../prisma/generated/client.js"
 import pg from "pg"
 
 const prismaClientSingleton = () => {
